03634b3bce48380ed7bc97c820adee0a1fa8d5b3
Al-amin Rahim <alamin102410@gmail.com>
2026-01-02 00:22:09 +0600

    modified config and deployed contracts in sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,34 @@
+// import "dotenv/config";
+// import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
+// import { defineConfig } from "hardhat/config";
+
+// export default defineConfig({
+//   plugins: [hardhatToolboxViemPlugin],
+//   solidity: {
+//     version: "0.8.28",
+//   },
+//   networks: {
+//     sepolia: {
+//       type: "http",
+//       url: process.env.SEPOLIA_RPC_URL!,
+//       accounts: [process.env.SEPOLIA_PRIVATE_KEY!],
+//     },
+//   },
+// });
+
 import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
-import { defineConfig } from "hardhat/config";
+import { configVariable, defineConfig } from "hardhat/config";
 
 export default defineConfig({
   plugins: [hardhatToolboxViemPlugin],
   solidity: {
     version: "0.8.28",
+  },
+  networks: {
+    sepolia: {
+      type: "http",
+      url: configVariable("SEPOLIA_RPC_URL"),
+      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    },
   },
 });
