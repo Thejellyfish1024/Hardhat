b879d179980c0c70d9efd235ef644aee058dfe83
Al-amin Rahim <alamin102410@gmail.com>
2026-01-12 23:21:48 +0600

    made a new func

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -2,32 +2,38 @@
 pragma solidity ^0.8.28;
 
 contract Counter {
-  uint public x;
+    uint public x;
 
-  event Increment(uint by);
-  event Decrement(uint by);
+    event Increment(uint by);
+    event Decrement(uint by);
+    event Multiply(uint by);
 
-  function inc() public {
-    x += 1; // 🐞 bug!
-    emit Increment(1);
-  }
+    function inc() public {
+        x += 1; // 🐞 bug!
+        emit Increment(1);
+    }
 
-  function incBy(uint by) public {
-    require(by > 0, "incBy: increment should be positive");
-    x += by;
-    emit Increment(by);
-  }
-  
-  function dec() public{
-    x -= 1;
-    emit Decrement(1);
-  }
+    function incBy(uint by) public {
+        require(by > 0, "incBy: increment should be positive");
+        x += by;
+        emit Increment(by);
+    }
 
-  function decBy(uint by) public{
-    require(by>0, "decBy: decrement should be positive");
-    x -= by;
-    emit Decrement(by);
-  }
+    function dec() public {
+        x -= 1;
+        emit Decrement(1);
+    }
+
+    function decBy(uint by) public {
+        require(by > 0, "decBy: decrement should be positive");
+        x -= by;
+        emit Decrement(by);
+    }
+
+    function multiplyBy(uint by) public {
+        require(by > 0, "multiplyBy: multiply should be positive");
+        x *= by;
+        emit Multiply(by);
+    }
 }
-
 // Random number to make this contract file unique: 961072473
